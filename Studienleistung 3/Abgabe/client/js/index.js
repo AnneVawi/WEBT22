24aa7e1872049d46a3635f8aa69875671d907d52
Lennart <northman@outlook.de>
2022-06-20 17:58:25 +0200

    loadDataFromServer

--- Studienleistung 3/Abgabe/client/js/index.js
+++ Studienleistung 3/Abgabe/client/js/index.js
@@ -1,7 +1,28 @@
-function loadDataFromServer(){
-  // Anmerkung: Hier ist fetch(url, {mode: "cors"}) zu nutzen.
+async function loadDataFromServer() {
+	const moduleGroupsResponse = await fetch('http://localhost:3000/module_groups', { mode: 'cors' });
+	const moduleGroups = await moduleGroupsResponse.json();
+
+	const modulesResponse = await fetch('http://localhost:3000/modules', { mode: 'cors' });
+	const modules = await modulesResponse.json();
+
+	const moduleMap = new Map();
+
+	moduleGroups.forEach((group) => {
+		moduleMap.set(group.acronym, {
+			acronym: group.acronym,
+			title: `Modulgruppe ${group.acronym}: ${group.title}`,
+			amount: 0
+		});
+	});
+
+	modules.forEach((module) => {
+		const item = moduleMap.get(module.moduleGroup);
+		if (item == null) return;
+		item.amount += 1;
+		moduleMap.set(item.acronym, item);
+	});
+
+	return Array.from(moduleMap.values());
 }
 
-function loadDataFromFile(){
-
-}
+loadDataFromServer().then(console.log);
---
+++ Studienleistung 3/README.md
@@ -0,0 +1,5 @@
+# Aufgaben
+
+-   Pie Chart bauen (D3) <- Anne
+-   Website Style (Bootstrap) <- Hendrik
+-   API calls <- Lennart
