8fccd34be5a6185254195a811e6c9f7491c75278
Lennart <northman@outlook.de>
2022-06-24 17:16:53 +0200

    abgabe 3

---
+++ Studienleistung 3/Abgabe/README.md
@@ -0,0 +1,13 @@
+# Studienleistung 3 - Team 1
+
+In diesem Ordner finden Sie die Abgabe des ersten Teams für die Studienleistung Nr. 3.
+
+Team 1 besteht aus folgenden Personen:
+
+-   Anne Abraham
+-   Hendrik Eilers
+-   Lennart Hoffmann
+
+Die Abgabe wurde in Google Chrome entwickelt und getestet.
+
+Die Anwendung lässt sich mit dem folgenden Befehl starten: `npm i && npm start`
--- Studienleistung 3/Abgabe/client/js/index.js
+++ Studienleistung 3/Abgabe/client/js/index.js
@@ -1,117 +1,114 @@
 class PieChart {
+	loadDataFromServer = async () => {
+		const moduleGroupsResponse = await fetch('http://localhost:3000/module_groups', { mode: 'cors' });
+		const moduleGroups = await moduleGroupsResponse.json();
 
-    loadDataFromServer = async () => {
-        const moduleGroupsResponse = await fetch('http://localhost:3000/module_groups', {mode: 'cors'});
-        const moduleGroups = await moduleGroupsResponse.json();
+		const modulesResponse = await fetch('http://localhost:3000/modules', { mode: 'cors' });
+		const modules = await modulesResponse.json();
 
-        const modulesResponse = await fetch('http://localhost:3000/modules', {mode: 'cors'});
-        const modules = await modulesResponse.json();
+		const moduleMap = new Map();
 
-        const moduleMap = new Map();
+		moduleGroups.forEach((group) => {
+			moduleMap.set(group.acronym, {
+				acronym: group.acronym,
+				title: `Modulgruppe ${group.acronym}: ${group.title}`,
+				amount: 0
+			});
+		});
 
-        moduleGroups.forEach((group) => {
-            moduleMap.set(group.acronym, {
-                acronym: group.acronym,
-                title: `Modulgruppe ${group.acronym}: ${group.title}`,
-                amount: 0
-            });
-        });
+		modules.forEach((module) => {
+			const item = moduleMap.get(module.moduleGroup);
+			if (item == null) return;
+			item.amount += 1;
+			moduleMap.set(item.acronym, item);
+		});
 
-        modules.forEach((module) => {
-            const item = moduleMap.get(module.moduleGroup);
-            if (item == null) return;
-            item.amount += 1;
-            moduleMap.set(item.acronym, item);
-        });
+		return Array.from(moduleMap.values());
+	};
 
-        return Array.from(moduleMap.values());
-    }
+	drawPieChart = (data) => {
+		const svg = d3.select('#pie-chart');
+		const width = 400;
+		const height = 400;
+		const radius = 200;
+		const ordinalScale = d3.scaleOrdinal().domain(data).range(['#4FE07C', '#E0C031', '#E0771B', '#E00804', '#1012E0', '#2FE0D6']);
 
-    drawPieChart = (data) => {
-        const svg = d3.select('#pie-chart');
-        const width = 400;
-        const height = 400;
-        const radius = 200;
-        const ordinalScale = d3.scaleOrdinal().domain(data).range(['#4FE07C', '#E0C031', '#E0771B', '#E00804', '#1012E0', '#2FE0D6']);
+		const tooltip = svg.append('div').attr('class', 'd3-tooltip');
+		tooltip.append('p').attr('class', 'label');
 
-        const tooltip = svg.append('div').attr('class', 'd3-tooltip');
-        tooltip.append('p').attr('class', 'label');
+		const pieChart = svg
+			.append('svg')
+			.attr('viewBox', `0 0 ${width} ${height}`)
+			.append('g')
+			.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+			.selectAll('arc')
+			.data(
+				d3.pie().value((d) => {
+					return d.amount;
+				})(data)
+			)
+			.enter();
 
-        const pieChart = svg
-            .append("svg")
-            .attr("viewBox", `0 0 ${width} ${height}`)
-            .append('g')
-            .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
-            .selectAll('arc')
-            .data(
-                d3.pie().value((d) => {
-                    return d.amount;
-                })(data)
-            )
-            .enter();
+		pieChart
+			.append('path')
+			.attr('d', d3.arc().outerRadius(radius).innerRadius(0))
+			.attr('fill', (d) => {
+				return ordinalScale(d.data.acronym);
+			})
+			.on('mousemove', (e, data) => {
+				//hier wurde absichtlich die Anzahl der Module mit angezeigt, da uns die Darstellung so hilfreicher vorkam
+				tooltip.select('.label').html(data.data.title + '<br>' + data.data.amount + ' Modul(e)');
+				tooltip
+					.style('display', 'block')
+					.style('left', e.pageX + 'px')
+					.style('top', e.pageY + 'px');
+			})
+			.on('mouseout', () => {
+				tooltip.style('display', 'none');
+			});
 
-        pieChart
-            .append('path')
-            .attr('d', d3.arc().outerRadius(radius).innerRadius(0))
-            .attr('fill', (d) => {
-                return ordinalScale(d.data.acronym);
-            })
-            .on('mousemove', (e, data) => {
-                tooltip.select('.label').html(data.data.title + "<br>" + data.data.amount + " Modul(e)");
-                tooltip
-                    .style('display', 'block')
-                    .style("left", e.pageX + "px")
-                    .style("top", e.pageY + "px")
-            })
-            .on('mouseout', () => {
-                tooltip.style('display', 'none');
-            });
-
-        pieChart
-            .append('text')
-            .attr('transform', (d) => {
-                return 'translate(' + d3.arc().outerRadius(radius).innerRadius(0).centroid(d) + ')';
-            })
-            .text((d) => {
-                return d.data.acronym;
-            })
-            .style('font-family', 'arial')
-            .style('font-size', 15);
-    }
+		pieChart
+			.append('text')
+			.attr('transform', (d) => {
+				return 'translate(' + d3.arc().outerRadius(radius).innerRadius(0).centroid(d) + ')';
+			})
+			.text((d) => {
+				return d.data.acronym;
+			})
+			.style('font-family', 'arial')
+			.style('font-size', 15);
+	};
 }
 
 class Nav {
+	links = document.getElementsByClassName('nav-link');
+	pieChart = new PieChart();
 
-    links = document.getElementsByClassName("nav-link")
-    pieChart = new PieChart()
+	init = () => {
+		this.loadTemplate(this.links[0]);
+		for (let link of this.links) {
+			link.addEventListener('click', () => {
+				this.loadTemplate(link);
+			});
+		}
+	};
 
-    init = () => {
-        this.loadTemplate(this.links[0])
-        for (let link of this.links) {
-            link.addEventListener("click", () => {
-                this.loadTemplate(link)
-            });
-        }
-    }
+	loadTemplate = (link) => {
+		const content = document.getElementById('content');
+		content.innerHTML = document.getElementById(link.id + '-template').innerHTML;
 
-    loadTemplate = (link) => {
-        const content = document.getElementById("content");
-        content.innerHTML = document.getElementById(link.id + "-template").innerHTML;
+		if (link.id === 'pie-link') {
+			this.pieChart.loadDataFromServer().then(this.pieChart.drawPieChart);
+		}
+		this.resetActive();
+		link.classList.add('active');
+	};
 
-        if (link.id === 'pie-link') {
-
-            this.pieChart.loadDataFromServer().then(this.pieChart.drawPieChart);
-        }
-        this.resetActive()
-        link.classList.add("active")
-    }
-
-    resetActive = () => {
-        for (let link of this.links) {
-            link.classList.remove("active")
-        }
-    }
-
+	resetActive = () => {
+		for (let link of this.links) {
+			link.classList.remove('active');
+		}
+	};
 }
 
 new Nav().init();
