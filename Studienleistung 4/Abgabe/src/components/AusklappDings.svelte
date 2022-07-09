8a8e90d24ef990e9657b29cae0f0b3d92e4569bd
Hendrik <info@hendrikeilers.de>
2022-07-10 01:15:46 +0200

    Pseudo hash IDs by name of Ausklappdings to avoid invalid characters

--- Studienleistung 4/Abgabe/src/components/AusklappDings.svelte
+++ Studienleistung 4/Abgabe/src/components/AusklappDings.svelte
@@ -1,15 +1,16 @@
 <script>
     export let stateName;
+    const token = btoa(stateName).replace(/[^\d\w\.]+/g,'');
 </script>
 
-<div class="accordion open" id="accordion{stateName}">
+<div class="accordion open" id="accordion{token}">
     <div class="accordion-item">
-        <h2 class="accordion-header" id="heading{stateName}">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{stateName}" aria-expanded="true" aria-controls="collapse{stateName}">
+        <h2 class="accordion-header" id="heading{token}">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{token}" aria-expanded="true" aria-controls="collapse{token}">
                 {stateName}
             </button>
         </h2>
-        <div id="collapse{stateName}" class="accordion-collapse collapse show" aria-labelledby="heading{stateName}" data-bs-parent="#accordion{stateName}">
+        <div id="collapse{token}" class="accordion-collapse collapse show" aria-labelledby="heading{token}" data-bs-parent="#accordion{token}">
             <div class="accordion-body">
                 <slot></slot>
             </div>
