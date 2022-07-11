a84d14364c0aad97de928ec2d482fdcb859c75f9
Lennart <northman@outlook.de>
2022-07-11 17:16:25 +0200

    Abgabe 4

--- Studienleistung 4/Abgabe/src/components/Card.svelte
+++ Studienleistung 4/Abgabe/src/components/Card.svelte
@@ -8,7 +8,7 @@
     <div class="card-header my-text-first">{town}
       </div>
     <div class="card-body">
-      <h6 class="card-subtitle mb-2 text-muted">
+      <h6 class="card-subtitle mb-2  my-text-first">
         Es existiert eine sehr geringe Gefahr eines Waldbrandes.
       </h6>
     </div>
@@ -18,7 +18,7 @@
     <div class="card-header my-text-second">{town}
       </div>
     <div class="card-body">
-      <h6 class="card-subtitle mb-2 text-muted">
+      <h6 class="card-subtitle mb-2 my-text-second">
         Es existiert eine geringe Gefahr eines Waldbrandes.
       </h6>
     </div>
@@ -28,17 +28,17 @@
     <div class="card-header my-text-third">{town}
       </div>
     <div class="card-body">
-      <h6 class="card-subtitle mb-2 text-muted">
+      <h6 class="card-subtitle mb-2 my-text-third">
         Es existiert eine mittlere Gefahr eines Waldbrandes.
       </h6>
     </div>
   </div>
   {:else if level === 4}
-  <div class="card my-border-forth mb-3">
-    <div class="card-header my-text-forth">{town}
+  <div class="card my-border-fourth mb-3">
+    <div class="card-header my-text-fourth">{town}
      </div>
     <div class="card-body">
-      <h6 class="card-subtitle mb-2 text-muted">
+      <h6 class="card-subtitle mb-2 my-text-fourth">
         Es existiert eine hohe Gefahr eines Waldbrandes.
       </h6>
     </div>
@@ -48,7 +48,7 @@
     <div class="card-header my-text-fifth">{town}
       </div>
     <div class="card-body">
-      <h6 class="card-subtitle mb-2 text-muted">
+      <h6 class="card-subtitle mb-2 my-text-fifth">
         Es existiert eine sehr hohe Gefahr eines Waldbrandes.
       </h6>
     </div>
@@ -58,28 +58,29 @@
 {/if}
 
   <style>
-      .card{
+    .card{
         width: 18rem;
         margin-bottom: 20px;
         margin-left: 20px;
         display: inline-block;
         flex-wrap: wrap;
-        justify-content: space-between;}
-    
+        justify-content: space-between;
+        background-color: #111111;
+        border: 3px solid;
+    }
     .my-border-first, .my-text-first{
-        color: rgb(120, 188, 120);
-        border-color: rgb(120, 188, 120);
+        color: #d0dfd0;
+        border-color: #d0dfd0;
     }
     .my-border-second, .my-text-second{
-        color: rgb(2, 127, 2);
-        border-color: rgb(2, 127, 2);
+       color: rgb(120, 188, 120);
+        border-color: rgb(120, 188, 120);
     }
-
     .my-border-third, .my-text-third{
         color: rgb(255, 213, 0);
         border-color: rgb(255, 213, 0);
     }
-    .my-border-forth, .my-text-forth{
+    .my-border-fourth, .my-text-fourth{
         color:rgb(253, 131, 0);
         border-color: rgb(253, 131, 0);
     }
